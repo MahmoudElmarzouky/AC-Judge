ca86f587897b0376b1702beeee0f223f617b5758
mahmoud magdy <mahmoud.amr147@gmail.com>
2021-05-24 04:20:11 +0200

    update Scripts , Login and Register files

--- Graduation Project/wwwroot/js/script.js
+++ Graduation Project/wwwroot/js/script.js
@@ -4,16 +4,14 @@ $(document).ready(function(){
 });
 
 function Show_User_Error_Message(element){
-    var btn = element + " form [type = 'submit']";
-    
-    $(btn).click(function(){
-        var alt = element + " div[id = 'user-error-msg']";
-        $(alt).each(function(){
-            var Msg = $(this).children('span');
-            if(!($(Msg).is(':empty'))){
-                $(this).css('display','block');
-            }
-        });
+    var alt = element + " div[id = 'user-error-msg']";
+    $(alt).each(function(){
+        var Msg = $(this).children('span');
+        if($(Msg).is(':empty')){
+            $(this).css('display','none');
+        }else{
+            $(this).css('display','block');
+        }
     });
 }
 
@@ -23,12 +21,6 @@ $(function(){
     /* Start User Sign In, Sign Up */;
     Show_User_Error_Message('.user-signin');
     Show_User_Error_Message('.user-signup');
-    
-
-   /*x.forEach(function(val){
-       
-        
-    });
     
     /* End User Sign In, Sign Up */
      
