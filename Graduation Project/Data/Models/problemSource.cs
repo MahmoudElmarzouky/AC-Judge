7e11b55893f5085c2373fefc4f7ec5b5d654e257
Mohamed-Elhabashy <mohamedsamy01090@gmail.com>
2021-06-18 22:36:38 +0200

    add-classes

---
+++ Graduation Project/Data/Models/problemSource.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Graduation_Project.Data.Models
+{
+    public class problemSource
+    {
+        public int ProblemId { get; set; }
+        
+        public string ProblemSource { get; set; }
+        
+        public int problemSourceId { get; set; }
+        
+        public string problemTitle { get; set; }
+        
+        public int problemType { get; set; } //interview or contest
+        
+        public int timelimit { get; set; }
+        
+        public int memorylimit { get; set; }
+        
+        public string inputType { get; set; }
+        
+        public string outputType { get; set; }
+       
+        public string problemText { get; set; }
+        
+        public int rating { get; set; }
+    }
+}
