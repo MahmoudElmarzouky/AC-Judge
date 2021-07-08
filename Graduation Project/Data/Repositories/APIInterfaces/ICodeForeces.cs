5a8afd596c36cb0ba08fc49b4f8c6933be1ec365
Mohamed-Elhabashy <mohamedsamy01090@gmail.com>
2021-07-08 18:34:55 +0200

    ApiInterface

---
+++ Graduation Project/Data/Repositories/APIInterfaces/ICodeForeces.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraduationProject.Data.Repositories.APIInterfaces
+{
+    interface ICodeForeces<T>
+    {
+        T GetProblem(string OnlineJudge,string ProblemId);
+        string GetVerdict(string OnlineJudge,string ProblemId,string solution);
+    }
+}
