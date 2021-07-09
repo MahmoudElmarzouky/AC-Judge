92e874618637ef116bd872a055e468cf5a8c26cd
mahmoud magdy <mahmoud.amr147@gmail.com>
2021-07-09 03:20:34 +0200

    Adding Blog to Profile

--- Graduation Project/Controllers/UserController.cs
+++ Graduation Project/Controllers/UserController.cs
@@ -32,21 +32,31 @@ namespace GraduationProject.Controllers
         }
 
         // GET: UserController/Details/5
+        [Route("User/Details/{id}")]
         public ActionResult Details(int id)
         {
             var currentuser = users.Find(id);
             TempData["userIdentity"] = user.UserIdentityId;
             return View(currentuser);
         }
-        public ActionResult OpentContest(int id)
+
+        [Route("User/MyProfile")]
+        public ActionResult Details()
+        {
+            var currentuser = users.Find(user.UserId);
+            TempData["userIdentity"] = user.UserIdentityId;
+            return View(currentuser);
+        }
+        public ActionResult OpenContest(int id)
         {
             var currentuser = users.Find(id);
             TempData["PrepeardBy"] = currentuser.UserName;
             return RedirectToAction("filter", "Contest");
         }
-        public ActionResult OpenBlog(int id)
+        public ActionResult OpenBlog()
         {
-            return RedirectToAction("filter", "Contest");
+            TempData["BlogsByUser"] = "UserBlogs";
+            return RedirectToAction("Index", "Blog");
         }
             // GET: UserController/Create
             public ActionResult Create()
