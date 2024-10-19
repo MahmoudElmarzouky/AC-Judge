363fc0350dc36de90d55fbd62e43d5a3c1d8c006
MohamedSameh <m.samehmohamed123@gmail.com>
2024-10-19 11:37:48 +0200

    migrate to dotnet 8

--- ACJudge/Migrations/20240210203536_init.Designer.cs
+++ ACJudge/Migrations/20240210203536_init.Designer.cs
@@ -13,7 +13,9 @@ namespace ACJudge.Migrations
 {
     [DbContext(typeof(EntitiesContext))]
     [Migration("20240210203536_init")]
+#pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
     partial class init
+#pragma warning restore CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
--- ACJudge/Migrations/20240210203536_init.cs
+++ ACJudge/Migrations/20240210203536_init.cs
@@ -5,7 +5,9 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace ACJudge.Migrations
 {
+#pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
     public partial class init : Migration
+#pragma warning restore CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
